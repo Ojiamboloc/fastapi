b8a9333d71c06f93e9dc24bc0b0bf72e0cc55f8a
Ojiamboloc <ojiamboloc@gmail.com>
2022-12-31 13:54:23 -0800

    added request body and post

--- myapi.py
+++ myapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI,Path
-
+from typing import Optional
+from pydantic import BaseModel
 app=FastAPI()
 students={
     1:{
@@ -12,6 +13,11 @@ students={
         "class":"Year 2"
     }
 }
+class Student(BaseModel):
+    name:str
+    age:int
+    year:str
+    
 #END POINT METHODS(get, post,put,delete)
 @app.get("/")
 def index():
@@ -19,10 +25,16 @@ def index():
 @app.get("/get-student/{student_id}")
 def get_student(student_id:int=Path(None,description="The ID of the student you wanna view",gt=0)):
     return students[student_id]
-@app.get("/get-by-name")
-def get_student(name:str):
+@app.get("/get-by-name/student_id")
+def get_student(*,student_id:int,name:Optional[str]=None,test:int):
     for student_id in students:
         if students[student_id]["name"]==name:
             return students[student_id]
     return {"Data":"Not Found"} 
-   
+   #Request Body and Post Method
+@app.post("/create-student/{student_id")
+def create_student(student_id:int, student:Student):
+    if student_id in students:
+        return {"Error": "Student Exists"}
+    students[student_id]=student
+    return students[student_id]   
