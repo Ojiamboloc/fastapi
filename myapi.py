f002051aeb7f365473d3c2359952277393465328
Ojiamboloc <ojiamboloc@gmail.com>
2022-12-31 14:13:46 -0800

    added delet method

--- myapi.py
+++ myapi.py
@@ -55,3 +55,10 @@ def update_student(student_id:int,student:UpdateStudent):
          students[student_id].year=student.year
                    
     return students[student_id]
+#delete method
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id:int):
+    if student_id not in students:
+        return {"Error": "Student does not exist"} 
+    del students[student_id]
+    return {"message": "Student Deleted"}
