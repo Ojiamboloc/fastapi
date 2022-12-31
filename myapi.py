8071d6531d0cb6e1fc8ad427e712dccec94a7523
Ojiamboloc <ojiamboloc@gmail.com>
2022-12-31 13:26:17 -0800

    added path attributes

--- myapi.py
+++ myapi.py
@@ -1,3 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path
+
 app=FastAPI()
+students={
+    1:{
+        "name": "ojiambo",
+        "age":17,
+        "class":"Year 1"
+    } ,2 :{
+        "name": "felix",
+        "age":18,
+        "class":"Year 2"
+    }
+}
 #END POINT METHODS(get, post,put,delete)
+@app.get("/")
+def index():
+    return {"name":"first data entry"}
+@app.get("/get-student/{student_id}")
+def get_student(student_id:int=Path(None,description="The ID of the student you wanna view")):
+    return students[student_id]
